c10666e6efc96c5d5609b0122cfe74c03dc4b1ae
stephen <stephenmoringjr@gmail.com>
2019-06-09 19:23:38 -0400

    Removing post functionality from blog project

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -103,12 +103,3 @@ def account():
 
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
-
-@app.route("/post/new", methods=['GET', 'POST'])
-@login_required
-def new_post():
-    form = PostForm()
-    if form.validate_on_submit():
-        flash('Your post has been created!', 'success')
-        return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
